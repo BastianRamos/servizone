503efb57d914c242916fbbec59468810b59098e5
BastianRamos <bastian2jz@gmail.com>
2024-06-04 13:33:47 -0400

    Avance en boton de redes de contacto

--- src/Components/ContactButton.jsx
+++ src/Components/ContactButton.jsx
@@ -1,9 +1,23 @@
 import { Box } from "@mui/material"
 
-export const ContactButton = ({ img, color, url }) => {
+
+export const ContactButton = ({ imgUrl, blurColor, contactUrl }) => {
+
+  const boxStyle = {
+    boxShadow: `0 0 4rem ${blurColor}`,
+    borderRadius: 2,
+    width: '80%',
+    marginY: 3,
+    padding: 1,
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    textAlign: 'center',
+    cursor: 'pointer',
+  }
+
   return (
-    <Box>
-        
+    <Box sx={boxStyle} onClick={() => alert('hola')}>
+      <img src={imgUrl} width='60%' />
     </Box>
   )
 }
--- src/Components/ContactUs.jsx
+++ src/Components/ContactUs.jsx
@@ -1,17 +1,34 @@
-import { Box } from "@mui/material"
+import { Box, Grid } from "@mui/material"
 import { ContactButton } from "./ContactButton"
 import { Section } from "./Section"
 
 
+const redes_contacto = [
+  { src: 'public/redes_contacto/facebook-logo.webp', color: '#1E90FF', link: 'https://www.facebook.com/profile.php?id=61557674667228&locale=es_LA' },
+  { src: 'public/redes_contacto/instagram-logo.webp', color: '#E1204E', link: 'https://www.instagram.com/servizone.cl/' },
+
+]
+
 export const ContactUs = () => {
   return (
     <Box className='marginForDesktop'>
-        <Section
-            key='contactanos'
-            texto='Contáctanos'
-            side="right"
-        />
-        <ContactButton />
+      <Section
+        key='contactanos'
+        texto='Contáctanos'
+        side="right"
+      />
+      <Grid container spacing={2}>
+        {redes_contacto.map((redContacto) => (
+          <Grid>
+            <ContactButton
+              key={redContacto.link}
+              imgUrl={redContacto.src}
+              contactUrl={redContacto.link}
+              blurColor={redContacto.color}
+            />
+          </Grid>
+        ))}
+      </Grid>
     </Box>
   )
 }
